f3181ae4ab61a3f50b67a083d13c7a542b6a6d1d
Ray Cerna <102126242+raycerna@users.noreply.github.com>
2022-07-22 09:15:37 -0500

    prepare funcs updated

--- prepare.py
+++ prepare.py
@@ -150,7 +150,8 @@ def prep_github_data(df, column = 'readme_contents', extra_words=[], exclude_wor
                             .apply(tokenize)\
                             .apply(remove_stopwords, 
                                    extra_words=extra_words, exclude_words=exclude_words)
-    df['clean'] = df['clean'].str.replace("&#9;","")
+
+    df['clean'] = df['clean'].str.replace("&#9;","") & df['clean'].str.replace("bot","") & df['clean'].str.replace("musicbot","")
 
     df['stemmed'] = df['clean'].apply(stem)
     
